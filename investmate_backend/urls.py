8654416c3de66e0fdff5e4459f978b2802aebbac
Jeffrey Zhang <jeffrey346@gmail.com>
2019-09-07 17:49:02 -0400

    Interface + user interaction functionality improved

--- investmate_backend/urls.py
+++ investmate_backend/urls.py
@@ -4,4 +4,7 @@ from investmate_backend import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('guide/', views.guide, name='guide'),
+    path('guide2/<sector>/', views.guide2, name='guide2'),
+    path('results/<sector>/<dog>/', views.results, name='results'),
 ]
--- investmate_backend/views.py
+++ investmate_backend/views.py
@@ -3,11 +3,36 @@ import sqlite3
 
 # Create your views here.
 def index(request):
+    return render(request, 'index.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def guide(request):
+    return render(request, 'guide.html', {})
+
+def guide2(request, sector):
+    # Create 4 dynamic urls:
+    # 1. Long-Term Growth
+    ltgURL = f"/results/{sector}/longTermGrowth/"
+    # 2. High Potential Return
+    hprURL = f"/results/{sector}/highPotentialReturn"
+    # 3. Currently Undervalued
+    cuURL = f"/results/{sector}/currentlyUndervalued"
+    # 4. Well-Roundedness
+    wrURL = f"/results/{sector}/wellRoundedness"
+    
+    urls = {'long': ltgURL, 'high_return':hprURL, 'currently_undervalued':cuURL,'well_rounded': wrURL}
+    return render(request, 'guide2.html', urls)
+
+def results(request, sector, dog):
+    '''
     conn = sqlite3.connect("db.sqlite3")
     cursor = conn.execute("SELECT * FROM investmate_backend_stock")
     rows = cursor.fetchall()
     args = {'test': rows}
-    return render(request, 'index.html', args)
-
-def about(request):
-    return render(request, 'about.html', {})
+    '''
+    sector += "dan"
+    args = {'test': sector + dog}
+    
+    return render(request, 'results.html', args)
